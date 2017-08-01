3012e7821ab37984c02cdc6a0bf5bd10b53b7197
nwre87 <nwre87@gmail.com>
2017-07-31 17:49:16 -1000

    stub out slider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
+import WorkContent from './components/WorkContent';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <div className="heading">
-          <h1 className="name">Nigel Earle</h1>
-          <h3 className="role">Full-Stack Software Developer</h3>
-          <hr/>
-          <p className="title">Professional Portfolio</p>
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="container">
+    <div className="heading">
+      <h1 className="name">Nigel Earle</h1>
+      <h3 className="role">Full-Stack Software Developer</h3>
+      <hr/>
+      <p className="title">Professional Portfolio</p>
+    </div>
+    <WorkContent />
+  </div>
+);
 
 export default App;
---
+++ src/components/WorkContent.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Slider from 'react-slick';
+
+import './WorkContent.css';
+
+const settings = {
+  dots: true,
+}
+
+const WorkContent = () => (
+  <div className="content">
+    <ul className="workList">
+      <li className="listItem">
+        <Slider {...settings}>
+          <div className="item"><img src="https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/Common-dog-behaviors-explained.jpg?itok=FSzwbBoi" alt=""/></div>
+          <div className="item"><img src="https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/Natural-Dog-Law-2-To-dogs%2C-energy-is-everything.jpg?itok=Z-ujUOUr" /></div>
+          <div className="item"><img src="https://www.petfinder.com/wp-content/uploads/2012/11/146067791-how-to-dog-bath-632x475.jpg"/></div>
+        </Slider>
+        <p>cultur guru</p>
+      </li>
+      <li className="listItem">
+        nest egg guru
+      </li>
+      <li className="listItem">
+        revolusun
+      </li>
+      <li className="listItem">
+        blueplanet
+      </li>
+    </ul>
+  </div>
+);
+
+export default WorkContent;
