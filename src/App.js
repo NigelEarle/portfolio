5305a3624868dad33eaae9371eef0d4d20a33c18
nwre87 <nwre87@gmail.com>
2017-08-01 14:31:44 -1000

    add description

--- src/App.js
+++ src/App.js
@@ -9,6 +9,19 @@ const App = () => (
       <h3 className="role">Full-Stack Software Developer</h3>
       <hr/>
       <p className="title">Professional Portfolio</p>
+      <div className="description">
+        <p>
+          Hello, I am a full-stack software developer specializing in JavaScript/ES6.
+          Some technologies I enjoy working with are Node.js, Next.js, React.js, Redux, MobX,
+          Webpack, Docker, Amazon Web Services and PostgreSQL.
+        </p>
+        <br/>
+        <p>
+          Building clean, stable and performant
+          is as important to me as the people I am building it with! Currently, I am looking to
+          contribute and be apart of a development team that shares same passion JavaScript web technologies that I do!
+        </p>
+      </div>
     </div>
     <WorkContent />
   </div>
