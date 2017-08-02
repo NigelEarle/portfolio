4d980e2c8f3d6976fe374b12008d723026b25d5c
nwre87 <nwre87@gmail.com>
2017-08-02 13:51:06 -1000

    add blueplanet

--- src/components/BluePlanetWork.js
+++ src/components/BluePlanetWork.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
-// import Slider from 'react-slick';
+import Slider from 'react-slick';
+
+import {
+  BLUEPLANET_1,
+  BLUEPLANET_2,
+  BLUEPLANET_3,
+  BLUEPLANET_4,
+  BLUEPLANET_5,
+  BLUEPLANET_6,
+  BLUEPLANET_7,
+  BLUEPLANET_8,
+} from '../assets/images';
+
 import './BluePlanetWork.css';
 
 export default class BluePlanetWork extends Component {
@@ -8,7 +20,18 @@ export default class BluePlanetWork extends Component {
   render() {
     return (
       <li className="listItem">
-        blueplanet
+        <Slider {...this.props.settings}>
+          <div className="item"><img src={BLUEPLANET_1} alt="work"/></div>
+          <div className="item"><img src={BLUEPLANET_2} alt="work"/></div>
+          <div className="item"><img src={BLUEPLANET_3} alt="work"/></div>
+          <div className="item"><img src={BLUEPLANET_4} alt="work"/></div>
+          <div className="item"><img src={BLUEPLANET_5} alt="work"/></div>
+          <div className="item"><img src={BLUEPLANET_6} alt="work"/></div>
+          <div className="item"><img src={BLUEPLANET_7} alt="work"/></div>
+          <div className="item"><img src={BLUEPLANET_8} alt="work"/></div>
+          
+        </Slider>
+        <p>cultur guru</p>
       </li>
     );
   }
