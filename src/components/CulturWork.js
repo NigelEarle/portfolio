4f3d41dd426c66720166617460181199a74f234f
nwre87 <nwre87@gmail.com>
2017-08-05 18:55:29 -1000

    edit cultur work

--- src/components/CulturWork.js
+++ src/components/CulturWork.js
@@ -18,26 +18,26 @@ export default class CulturWork extends Component {
           <div className="item"><img src={CULTUR_1} alt="work"/></div>
           <div className="item"><img src={CULTUR_2} alt="work"/></div>
         </Slider>
-        <h1>Cultur</h1>
+        <h1>Cultur Guru (RESTful API)</h1>
         <div className="experience">
           <p>
             The Cultur Guru API was my first full-scale project I had the pleasure of working
             on for a little over 10 months. The API, written in Ruby on Rails, was responsible
             for scraping event data across 25+ different sources. The API had already been 
-            built out to an extent before our team jumped on. Our back end team was responsible for developing a new 
-            server infrastructure and deployment strategy - focusing on load balancing and back end processes, building
-            additional features and squashing an existing bugs. Operating on a remote team of 7+
-            people gave me the experience and wisdom of working in teams through pair programming
-            and meeting tough deadlines.
+            built out to an extent by a previous devlopment team before our back-end team jumped on. We were
+            responsible for developing a new server infrastructure and deployment strategy - focused on load
+            balancing and back end processes, building additional features and squashing an existing bugs.
+            Operating on a remote team of 7+ people gave me the initial experience and wisdom of working in
+            teams through pair programming and meeting tough deadlines.
           </p>
           <br/>
           <p>
             My main responsibilities for this position were building and configuring the server
             infrastructure across several different environments and micro-service workers, as
-            well as adding and debugging existing features. Under the supervision of the senior
+            well as adding and debugging existing features. Under the supervision of my senior
             engineer, we were able deploy to these environments and workers with ease using Digital
             Ocean, Docker, Puma and Ansible. An issue our deployment strategy solved was optimizing
-            image processing of 10,000+ images. We figured that instead of scaling vertically by
+            image processing of 700,000+ images. We figured that instead of scaling vertically by
             giving the existing workers more memory and ram, we could scale them horizontally by
             creating more servers with a minimum amount of memory and ran. This ended up costing the
             administrators and owners less with the most optimal processing power.
