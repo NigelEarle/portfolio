fbc769412e13f657f34190a7b77e36f0878957ef
nwre87 <nwre87@gmail.com>
2017-08-07 17:03:38 -1000

    add show more and show less conditional

--- src/components/BluePlanetWork.js
+++ src/components/BluePlanetWork.js
@@ -16,8 +16,9 @@ import './BluePlanetWork.css';
 
 export default class BluePlanetWork extends Component {
   state = { showMore: false };
-  
+
   render() {
+    const { showMore } = this.state;
     return (
       <li className="listItem">
         <Slider {...this.props.settings}>
@@ -38,25 +39,37 @@ export default class BluePlanetWork extends Component {
             front-end portals), styling of React components using CSS-Modules and developing
             the Manage Billing pages for the project owner portal using React.js and Redux.
           </p>
-          <br/>
-          <p>
-            Working on the component library gave me the mindset of thinking about how a
-            developer should be using and interacting with the tools and libraries available.
-            We built out this dynamic component library to cut down on code redundancy throughout
-            the front end portals, given that a lot of the same React components would be used throughout.
-            Some of the dynamic UI components we developed were tables, search inputs, radio buttons,
-            inputs (with validation), drop-downs, buttons, lists and list-items. Pairing with two
-            other very experienced in-house developers on this project allowed us to streamline our
-            development process and start using these dynamic components in production relatively quickly.
-          </p>
-          <br/>
-          <p>
-            One of the larger tasks I was assigned to was the Manage Billing pages for the
-            project owner, front-end portal. This section of the project owner portal was
-            responsible for setting up billing specifications for homeowners - i.e. billing cycles
-            and plans, flat rate fees, additional fees and taxes, kilowatt-hour usage tiers. Using
-            Redux as a global state manager for these components and pages made huge difference while
-            developing, which allowed for the usage of dynamic data outside the scope of a single component hierarchy.
+          { showMore &&
+            <div>
+              <br/>
+              <p>
+                Working on the component library gave me the mindset of thinking about how a
+                developer should be using and interacting with the tools and libraries available.
+                We built out this dynamic component library to cut down on code redundancy throughout
+                the front end portals, given that a lot of the same React components would be used throughout.
+                Some of the dynamic UI components we developed were tables, search inputs, radio buttons,
+                inputs (with validation), drop-downs, buttons, lists and list-items. Pairing with two
+                other very experienced in-house developers on this project allowed us to streamline our
+                development process and start using these dynamic components in production relatively quickly.
+              </p>
+              <br/>
+              <p>
+                One of the larger tasks I was assigned to was the Manage Billing pages for the
+                project owner, front-end portal. This section of the project owner portal was
+                responsible for setting up billing specifications for homeowners - i.e. billing cycles
+                and plans, flat rate fees, additional fees and taxes, kilowatt-hour usage tiers. Using
+                Redux as a global state manager for these components and pages made huge difference while
+                developing, which allowed for the usage of dynamic data outside the scope of a single component hierarchy.
+              </p>
+            </div>
+          }
+          <p
+            className="showMore"
+            onClick={
+              () => this.setState({ showMore: !showMore})
+            }
+          >
+          {!showMore ? 'Show more...' : 'Show less...'}
           </p>
         </div>
       </li>
